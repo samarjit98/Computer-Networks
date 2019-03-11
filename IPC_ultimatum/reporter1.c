71c26ffb3c506ed60b055b676b0bd028b5d87677
samarjit98 <karmakar.samarjit@gmail.com>
2019-03-11 16:19:43 +0530

    the ipc god

---
+++ IPC_ultimatum/reporter1.c
@@ -0,0 +1,21 @@
+#include "ipc.h"
+
+int main(int argc, char* argv[])
+{
+	char* pathname = "./fifo_rep_1";
+	mkfifo(pathname, 0666);
+
+	int fd = open(pathname, O_RDWR);
+
+	while(1)
+	{
+		char buff[20];
+		scanf("%s", buff);
+		//printf("Before dup\n");
+		//dup2(fd, fileno(stdout));
+		//printf("After dup\n");
+		write(fd, buff, strlen(buff)+1);
+	}
+
+	return 0;
+}
